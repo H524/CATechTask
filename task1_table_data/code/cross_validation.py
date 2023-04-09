56f02257e9bbed06e0756cdc92daa5423e79790e
hiroki-konishi <watasekoni6@gmail.com>
2023-04-09 17:38:59 +0000

    コマンドライン引数対応

--- task1_table_data/code/cross_validation.py
+++ task1_table_data/code/cross_validation.py
@@ -1,7 +1,9 @@
-# import
+import os
+import time
 import logging
-from sklearn.model_selection import cross_val_score
+from pprint import pprint
 import numpy as np
+from sklearn.model_selection import cross_val_score, StratifiedKFold, cross_validate
 
 # logging
 logger = logging.getLogger(__name__)
@@ -13,12 +15,31 @@ logger.addHandler(file_handler)
 
 def cross_validation(modelname, model, dataset):
     """交差検証を行う処理"""
+    start_time = time.perf_counter()
     logging.info('start cross validation for %s', modelname)
-    print('start cross validation for %s', modelname)
-    scores = cross_val_score(model, dataset.data, dataset.target, cv=5)
-#   np.savetxt(modelname + 'output_cv.csv', scores) 
-#   np.savetxt(modelname + 'output_ave.csv', [np.mean(scores)])
-    print(f'{modelname} Cross-Validation scores: {scores}')
-    print(f'{modelname} Average score: {np.mean(scores)}')
+    print(f'start cross validation for {modelname}')
+
+    # scores = cross_val_score(model, dataset.data, dataset.target, cv=5)
+    scoring = {"p": "precision_macro",
+                "r": "recall_macro",
+                "f": "f1_macro"}
+    skf = StratifiedKFold(n_splits=5, shuffle=False)
+    scores = cross_validate(model, dataset.data, dataset.target, cv=skf, scoring=scoring)
+
+    pprint(scores)
+
+    os.makedirs(f'output/{modelname}', exist_ok=True)
+    # np.savetxt(f'output/{modelname}_cv.csv', np.round(scores, decimals=3))
+    np.savetxt(f'output/{modelname}/{modelname}_fit_time.csv', np.round(scores['fit_time'], decimals=3))
+    np.savetxt(f'output/{modelname}/{modelname}_score_time.csv', np.round(scores['score_time'], decimals=3))
+    np.savetxt(f'output/{modelname}/{modelname}_test_f.csv', np.round(scores['test_f'], decimals=3))
+    np.savetxt(f'output/{modelname}/{modelname}_test_p.csv', np.round(scores['test_p'], decimals=3))
+    np.savetxt(f'output/{modelname}/{modelname}_test_r.csv', np.round(scores['test_r'], decimals=3))
+    # np.savetxt(f'output/{modelname}_ave.csv', [round(np.mean(scores), 3)])
+
+    # print(f'{modelname} Cross-Validation scores: {np.round(scores, decimals=3)}')
+    # print(f'{modelname} Average score: {np.mean(scores):.3f}')
+    print(f'Execution time: {time.perf_counter() - start_time:.1f}(s)')
+
     logging.info('end cross validation for %s', modelname)
-    print('end cross validation for %s', modelname)
+    print(f'end cross validation for {modelname}')
--- task1_table_data/code/task1.py
+++ task1_table_data/code/task1.py
@@ -1,9 +1,8 @@
-# logging
+import sys
 import logging
-
-# import
-# dataset
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.svm import SVC, LinearSVC
 from sklearn.tree import DecisionTreeClassifier
@@ -13,14 +12,9 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import GradientBoostingClassifier
 
 # util
-# from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LinearRegression
-# from sklearn.metrics import accuracy_score
-# from sklearn.metrics import precision_score
-# from sklearn.metrics import recall_score
-# from sklearn.metrics import classification_report
 from cross_validation import cross_validation
 
+arr = sys.argv[1:]
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -29,49 +23,108 @@ file_handler = logging.FileHandler('test.log')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
-def task1():
+def task1(select_model_arr):
     """メイン処理"""
     # load dataset
     logging.info('start load dataset')
     print('start load dataset')
-    dataset = datasets.fetch_covtype()
+    dataset = datasets.fetch_covtype(data_home="./data")
     # x_train, x_test, t_train, t_test = train_test_split(dataset.data, dataset.target, test_size=0.3, random_state=0)
 
-    # load model
-    logging.info('start load model')
-    print('tart load model')
-    logreg = LogisticRegression()
-    svc = SVC()
-    linear_svc = LinearSVC()
-    decisiontree = DecisionTreeClassifier()
-    randomforest = RandomForestClassifier()
-    knn = KNeighborsClassifier()
-    gaussian = GaussianNB()
-    gbk = GradientBoostingClassifier()
-    sgd = SGDClassifier()
+    logging.info('start cross validation')
+    print('start cross validation')
 
-    logging.info('start larning')
-    print('start larning')
-    # ロジスティック回帰
-    cross_validation('logreg', logreg, dataset)
-    # SVM
-    cross_validation('svc', svc, dataset)
-    # LinearSVC
-    cross_validation('linear_svc', linear_svc, dataset)
-    # 決定木
-    cross_validation('decisiontree', decisiontree, dataset)
-    # ランダムフォレスト
-    cross_validation('randomforest', randomforest, dataset)
-    # KNeighborsClassifier
-    cross_validation('knn', knn, dataset)
-    # ナイーブベイズ
-    cross_validation('gaussian', gaussian, dataset)
-    # GradientBoostingClassifier
-    cross_validation('gbk', gbk, dataset)
-    # Stochastic Gradient Descent
-    cross_validation('sgd', sgd, dataset)
-    logging.info('end larning')
-    print('end larning')
+    if select_model_arr in 'logreg':
+        # ロジスティック回帰
+        logreg = LogisticRegression()
+        cross_validation('logreg', logreg, dataset)
+
+    if select_model_arr in 'svc':
+        # SVM
+        svc = SVC()
+        cross_validation('svc', svc, dataset)
+
+    if select_model_arr in 'linear_svc':
+        # LinearSVC
+        linear_svc = LinearSVC()
+        cross_validation('linear_svc', linear_svc, dataset)
+
+    if select_model_arr in 'decisiontree':
+        # 決定木
+        decisiontree = DecisionTreeClassifier()
+        cross_validation('decisiontree', decisiontree, dataset)
+
+    if select_model_arr in 'randomforest':
+        # ランダムフォレスト
+        randomforest = RandomForestClassifier()
+        cross_validation('randomforest', randomforest, dataset)
+ 
+    if select_model_arr in 'knn':
+        # K-近傍法
+        knn = KNeighborsClassifier()
+        cross_validation('knn', knn, dataset)
+
+    if select_model_arr in 'gaussian':
+        # ナイーブベイズ
+        gaussian = GaussianNB()
+        cross_validation('gaussian', gaussian, dataset)
+
+    if select_model_arr in 'gbk':       
+        # 勾配ブースティング
+        gbk = GradientBoostingClassifier()
+        cross_validation('gbk', gbk, dataset)
+
+    if select_model_arr in 'logreg':
+        # 確率的勾配降下法
+        sgd = SGDClassifier()
+        cross_validation('sgd', sgd, dataset)
+
+    logging.info('end cross validation')
+    print('end cross validation')
+
+
+
+
+
+
+
+
+
+    # logging.info('end larning')
+    # print('end larning')
+
+    # logging.info('start fitting')
+    # print('start fitting')
+
+    # logging.info('fitting logreg')
+    # print('fitting logreg')
+    # # ロジスティック回帰
+    # logreg.fit(x_train, t_train)
+    # cross_validation('logreg_fit', logreg, dataset)
+    # logreg_pred = logreg.predict(x_test) 
+    # print('logreg_report_fit')
+    # print(classification_report(t_test, logreg_pred))
+
+    # logging.info('fitting decisiontree')
+    # print('fitting decisiontree')
+    # # 決定木
+    # decisiontree.fit(x_train, t_train)
+    # cross_validation('decisiontree_fit', decisiontree, dataset)
+    # decisiontree_pred = decisiontree.predict(x_test)
+    # print('logreg_fit_report')
+    # print(classification_report(t_test, decisiontree_pred))
+
+    # logging.info('fitting randomforest')
+    # print('fitting randomforest')
+    # # ランダムフォレスト
+    # randomforest.fit(x_train, t_train)
+    # cross_validation('randomforest_fit', randomforest, dataset)
+    # randomforest_pred = randomforest.predict(x_test)
+    # print('logreg_fit_report')
+    # print(classification_report(t_test, randomforest_pred))
+
+    # logging.info('end fitting')
+    # print('end fitting')
     return
 
-task1()
+task1(arr)
